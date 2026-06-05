d388293e145d0dba2f9a4d3906dee932627a8693
evitorsilva <ceja.vitorgabriel.e.silva@gmail.com>
2026-06-05 16:14:45 -0300

    SECRETS

--- src/main/java/org/evitorsilva/services/JwtService.java
+++ src/main/java/org/evitorsilva/services/JwtService.java
@@ -4,7 +4,9 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import jakarta.annotation.PostConstruct;
 import org.jspecify.annotations.NonNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +14,14 @@ import java.util.Set;
 
 @Service
 public class JwtService {
+    @Value("${api.security.token.secret}")
+    private String tokenSecret;
+    private Algorithm algorithm;
 
-    private final Algorithm algorithm = Algorithm.HMAC256("HS256");
+    @PostConstruct
+    public void init() {
+        this.algorithm = Algorithm.HMAC256(tokenSecret);
+    }
 
     public String create(String name, @NonNull Set<GrantedAuthority> roles) {
         String[] rolesGet = roles.stream()
